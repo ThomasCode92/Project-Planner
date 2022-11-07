9b7ec0a94152fc8cbb093cbe9edc3962221f113e
ThomasCode92 <thomascode92@hotmail.com>
2022-11-07 21:29:12 +0100

    Starting with the "Switch Project" Logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,39 @@
 class Tooltip {}
 
-class ProjectItem {}
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+
+    this.connectSwitchButton();
+    this.connectMoreInfoButton();
+  }
+
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector('button:last-of-type');
+
+    switchBtn.addEventListener('click');
+  }
+
+  connectMoreInfoButton() {}
+}
 
 class ProjectList {
+  projects = [];
+
   constructor(type) {
     const projectItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const projectItem of projectItems) {
+      this.projects.push(new ProjectItem(projectItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject() {}
+
+  switchProject(projectId) {
+    const projectIndex = this.projects.findIndex(p => p.id === projectId);
+    this.projects.splice(projectIndex, 1);
   }
 }
 
