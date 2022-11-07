96e765212811f01f13f601b3a002b4bfd337e20a
ThomasCode92 <thomascode92@hotmail.com>
2022-11-07 21:49:05 +0100

    Passing Method References Around

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,9 @@
 class Tooltip {}
 
 class ProjectItem {
-  constructor(id) {
+  constructor(id, updateProjectLists) {
     this.id = id;
+    this.updateProjectLists = updateProjectLists;
 
     this.connectSwitchButton();
     this.connectMoreInfoButton();
@@ -12,7 +13,7 @@ class ProjectItem {
     const projectItemElement = document.getElementById(this.id);
     const switchBtn = projectItemElement.querySelector('button:last-of-type');
 
-    switchBtn.addEventListener('click');
+    switchBtn.addEventListener('click', this.updateProjectLists);
   }
 
   connectMoreInfoButton() {}
@@ -22,17 +23,28 @@ class ProjectList {
   projects = [];
 
   constructor(type) {
+    this.type = type;
+
     const projectItems = document.querySelectorAll(`#${type}-projects li`);
     for (const projectItem of projectItems) {
-      this.projects.push(new ProjectItem(projectItem.id));
+      this.projects.push(
+        new ProjectItem(projectItem.id, this.switchProject.bind(this))
+      );
     }
     console.log(this.projects);
   }
 
-  addProject() {}
+  setSwitchHandler(switchHandler) {
+    this.switchHandler = switchHandler;
+  }
+
+  addProject() {
+    console.log(this);
+  }
 
   switchProject(projectId) {
     const projectIndex = this.projects.findIndex(p => p.id === projectId);
+    this.switchHandler(this.projects.find(p => p.id === projectId));
     this.projects.splice(projectIndex, 1);
   }
 }
@@ -41,6 +53,13 @@ class App {
   static init() {
     const activeProjectList = new ProjectList('active');
     const finishedProjectList = new ProjectList('finished');
+
+    activeProjectList.setSwitchHandler(
+      finishedProjectList.addProject.bind(finishedProjectList)
+    );
+    finishedProjectList.setSwitchHandler(
+      activeProjectList.addProject.bind(activeProjectList)
+    );
   }
 }
 
