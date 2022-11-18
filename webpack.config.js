aceaeaddd85e768652e1a573a9782ab4e9d8bca7
ThomasCode92 <thomascode92@hotmail.com>
2022-11-18 16:01:00 +0100

    Using webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,11 @@ module.exports = {
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'assets', 'scripts'),
+    publicPath: '/assets/scripts/',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname),
+    },
   },
 };
